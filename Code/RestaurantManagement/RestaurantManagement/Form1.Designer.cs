d5d22e22cb66b519a1267a55257a30b1800aa4d2
Minh Tu Nguyen <88047847+MinhTuMTN@users.noreply.github.com>
2022-10-15 14:36:44 +0700

    ...

--- Code/RestaurantManagement/RestaurantManagement/Form1.Designer.cs
+++ Code/RestaurantManagement/RestaurantManagement/Form1.Designer.cs
@@ -72,7 +72,7 @@
             this.rjTextBox1.BorderSize = 2;
             this.rjTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rjTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.rjTextBox1.Location = new System.Drawing.Point(13, 218);
+            this.rjTextBox1.Location = new System.Drawing.Point(13, 177);
             this.rjTextBox1.Margin = new System.Windows.Forms.Padding(4);
             this.rjTextBox1.Multiline = false;
             this.rjTextBox1.Name = "rjTextBox1";
