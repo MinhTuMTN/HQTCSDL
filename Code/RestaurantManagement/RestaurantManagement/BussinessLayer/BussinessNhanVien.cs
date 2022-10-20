283c2fd3d4c4c200c8c6262847d314d9b9c6b892
Minh Tu Nguyen <88047847+MinhTuMTN@users.noreply.github.com>
2022-10-20 11:56:31 +0700

    Thêm tính năng tìm kiếm và thêm nhân viên

--- Code/RestaurantManagement/RestaurantManagement/BussinessLayer/BussinessNhanVien.cs
+++ Code/RestaurantManagement/RestaurantManagement/BussinessLayer/BussinessNhanVien.cs
@@ -3,6 +3,7 @@ using RestaurantManagement.DataAccessLayer.Model;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,31 @@ namespace RestaurantManagement.BussinessLayer
             DataTable results = new DataTable();
             results = connection.MyExecuteQueryDataTable("SELECT * FROM NhanVien", CommandType.Text, ref error);
             return results;
+        }
+
+        public DataTable FindNhanVien(string text, ref string error)
+        {
+            DataTable results = new DataTable();
+            string cmd = "SELECT * FROM dbo.fnSearchNhanVien(@text)";
+            SqlParameter parameter = new SqlParameter("@text", text);
+            results = connection.MyExecuteQueryDataTable(cmd, CommandType.Text, ref error, parameter);
+            return results;
+        }
+
+        public bool AddNhanVien(NhanVien nhanVien, ref string error)
+        {
+            string cmd = "dbo.spInsertNhanVien";
+            SqlParameter[] parameters = {
+                new SqlParameter("@ma", nhanVien.MaNhanVien),
+                new SqlParameter("@ten", nhanVien.HoTen),
+                new SqlParameter("@ngaySinh", nhanVien.NgaySinh),
+                new SqlParameter("@gioiTinh", nhanVien.GioiTinh),
+                new SqlParameter("@diaChi", nhanVien.DiaChi),
+                new SqlParameter("@SDT", nhanVien.SoDienThoai),
+                new SqlParameter("@heSoLuong", nhanVien.HeSoLuong),
+                new SqlParameter("@loaiNhanVien", nhanVien.LoaiNhanVien)
+            };
+            return connection.MyExecuteNonQuery(cmd, CommandType.StoredProcedure, ref error, parameters);
         }
     }
 }
