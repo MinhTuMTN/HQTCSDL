f6ce0f4086b02b870d723f616c790a2756ff8efa
Minh Tu Nguyen <88047847+MinhTuMTN@users.noreply.github.com>
2022-10-20 11:54:47 +0700

    Thêm và thay đổi một vài thuộc tính

--- Code/RestaurantManagement/RestaurantManagement/DataAccessLayer/Model/NhanVien.cs
+++ Code/RestaurantManagement/RestaurantManagement/DataAccessLayer/Model/NhanVien.cs
@@ -11,26 +11,35 @@ namespace RestaurantManagement.DataAccessLayer.Model
         private string maNhanVien;
         private string hoTen;
         private string soDienThoai;
-        private string ngaySinh;
+        private DateTime ngaySinh;
         private bool gioiTinh;
+        private string diaChi;
+        private float heSoLuong;
+        private string loaiNhanVien;
 
         public NhanVien()
         {
         }
 
-        public NhanVien(string maNhanVien, string hoTen, string soDienThoai, string ngaySinh, bool gioiTinh)
+        public NhanVien(string maNhanVien, string hoTen, string soDienThoai, DateTime ngaySinh, bool gioiTinh, string diaChi, float heSoLuong, string loaiNhanVien)
         {
             this.maNhanVien = maNhanVien;
             this.hoTen = hoTen;
             this.soDienThoai = soDienThoai;
             this.ngaySinh = ngaySinh;
             this.gioiTinh = gioiTinh;
+            this.diaChi = diaChi;
+            this.heSoLuong = heSoLuong;
+            this.loaiNhanVien = loaiNhanVien;
         }
 
         public string MaNhanVien { get => maNhanVien; set => maNhanVien = value; }
         public string HoTen { get => hoTen; set => hoTen = value; }
         public string SoDienThoai { get => soDienThoai; set => soDienThoai = value; }
-        public string NgaySinh { get => ngaySinh; set => ngaySinh = value; }
+        public DateTime NgaySinh { get => ngaySinh; set => ngaySinh = value; }
         public bool GioiTinh { get => gioiTinh; set => gioiTinh = value; }
+        public string DiaChi { get => diaChi; set => diaChi = value; }
+        public float HeSoLuong { get => heSoLuong; set => heSoLuong = value; }
+        public string LoaiNhanVien { get => loaiNhanVien; set => loaiNhanVien = value; }
     }
 }
