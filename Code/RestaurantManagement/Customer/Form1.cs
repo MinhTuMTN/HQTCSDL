d0ad649f6052c73cde1ab952af5b125a01995980
DiDi0703 <110220867+DiDi0703@users.noreply.github.com>
2022-11-16 22:29:15 +0700

    frm DatTruoc

---
+++ Code/RestaurantManagement/Customer/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Customer
+{
+    public partial class frmDatTruoc : Form
+    {
+        BusinessDatTruoc business = new BusinessDatTruoc();
+        public frmDatTruoc()
+        {
+            InitializeComponent();
+
+            string curDir = Directory.GetCurrentDirectory();
+            //browser.Url = new Uri(String.Format("file:///{0}/map.html", curDir));
+            //browser.ScriptErrorsSuppressed = false;
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            string maKhachHang = business.GetMaKhachHang(ref error);
+            if(maKhachHang == null)
+            {
+                
+            }
+        }
+    }
+}
